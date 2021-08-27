1c577ebd86b7311817d6253ce66f0f223fc5d28c
narenm1234 <naren09072020@gmail.com>
2021-08-28 00:05:57 +0530

    Sample Project
    Sample Project

---
+++ src/AppRouter.js
@@ -0,0 +1,42 @@
+import React, { useReducer, useEffect } from "react";
+import Products from "./Products";
+import { Routes, Route } from "react-router-dom";
+import Detail from "./Detail";
+import Cart from "./Cart";
+import Checkout from "./Checkout";
+import cartReducer from "./cartReducer";
+
+
+let initialCart;
+try {
+  initialCart = JSON.parse(localStorage.getItem("cart")) ?? [];
+} catch {
+  console.error("The cart could not be parsed into JSON.");
+  initialCart = [];
+}
+
+export default function AppRouter(){
+
+  const [cart, dispatch] = useReducer(cartReducer, initialCart);
+
+  useEffect(() => localStorage.setItem("cart", JSON.stringify(cart)), [cart]);
+
+    return(
+        <Routes>
+            <Route path="/" element={<h1>Welcome to Carved Rock Fitness</h1>} />
+            <Route path="/:category" element={<Products />} />
+            <Route
+              path="/:category/:id"
+              element={<Detail dispatch={dispatch} />}
+            />
+            <Route
+              path="/cart"
+              element={<Cart cart={cart} dispatch={dispatch} />}
+            />
+            <Route
+              path="/checkout"
+              element={<Checkout cart={cart} dispatch={dispatch} />}
+            />
+          </Routes>
+    )
+}
